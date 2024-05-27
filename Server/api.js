faa8895d05b5fb61f878b8ad9555500a89533f99
Thoq <125930852+Thoq-jar@users.noreply.github.com>
2024-05-27 14:46:50 -0400

    Update api.js

--- Server/api.js
+++ Server/api.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const https = require('https');
 
 const app = express();
 const port = 3000;
@@ -21,4 +23,28 @@ app.post("/send-location", (req, res) => {
     message: "Location received successfully!",
     latitude: location.latitude,
     longitude: location.longitude,
-  })
+  });
+});
+
+app.get("/display-location", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Client", "display_location.html"));
+});
+
+const serverOptions = {
+  key: fs.readFileSync('localhost.key'),
+  cert: fs.readFileSync('localhost.crt')
+};
+
+const server = https.createServer(serverOptions, app);
+server.listen(port, () => {
+  console.log(`Server is listening at https://pi.local:${port}`);
+});
+
+process.on("SIGINT", () => {
+  console.log("Server is shutting down...");
+  server.close(() => {
+    console.log("Server has been shut down.");
+    process.exit(0);
+  });
+});
+
